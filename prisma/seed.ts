3992a1c722ab3bfc82fd576d9f672dad4944c927
Jansen Ken Pegrasio <kenpegrasio@gmail.com>
2025-04-06 14:35:07 +0800

    Add more sample data for seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,16 +6,31 @@ async function main() {
     data: [
       { email: "admin@servihub.com", role: "admin", name: "Admin User" },
       { email: "user1@servihub.com", name: "User One" },
+      { email: "user2@servihub.com", name: "User Two " },
     ],
   });
 
-  await prisma.report.create({
-    data: {
-      type: "review",
-      target_id: 101,
-      reason: "Spam content",
-      submitted_by: 2,
-    },
+  await prisma.report.createMany({
+    data: [
+      {
+        type: "review",
+        target_id: 101,
+        reason: "Spam content",
+        submitted_by: 2,
+      },
+      {
+        type: "other",
+        target_id: 105,
+        reason: "Harassment",
+        submitted_by: 3,
+      },
+      {
+        type: "business",
+        target_id: 105,
+        reason: "Business Trip",
+        submitted_by: 2,
+      },
+    ],
   });
   console.log("Database seeded");
 }
